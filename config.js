834bf4a8b9d2802ee23c0e420c1d26b862721cd3
arukh89 <85455057+arukh89@users.noreply.github.com>
2025-09-22 20:48:52 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// ganti URL ini kalau backend Replit berubah
+const BACKEND_URL = "https://1a4f1f38-1bc5-459f-89d3-7e411642339d-00-8cgu1jweczd6.sisko.replit.dev";
+
+// export untuk Node.js (kalau dibutuhkan)
+if (typeof module !== "undefined") {
+  module.exports = { BACKEND_URL };
+}
